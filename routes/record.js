8869021975eda27bb498c873c1255551a60e3ef0
TGY-forture <1981472184@qq.com>
2020-07-30 22:55:46 +0800

    record information

--- routes/record.js
+++ routes/record.js
@@ -53,25 +53,75 @@ router.get('/', function (req, res, next) {
   )
 })
 
-router.post('/', function (req, res, next) {
-  let data = req.body
-  let dbname = data.dbname
-  let seq = data.seq
-  let tmparr = []
-  for (let key in data) {
-    if (key !== 'seq' && key !== 'dbname')
-    tmparr.push(`${key}='${data[key]}'`)
-  }
-  let sql = `UPDATE ${dbname} SET ${tmparr.join(',')} WHERE id>1 AND seq='${seq}'`
+router.get('/data', (req, res, next) => {
+  let seq = req.query.seq;
+  let company = req.query.company
+  let sql = `SELECT * FROM stateflash WHERE seq='${seq}' AND company='${company}'`
+  let conn = createConn()
+  conn.query(sql, (err, results, fields) => {
+    conn.end()
+    if (!err) {
+      res.send(results)
+    } else {
+      res.send('fail')
+    }
+  })
+})
+
+router.put('/', (req, res, next) => {
+  let seq = req.body.seq 
+  let table = req.body.table
+  let sql = `INSERT INTO ${table} (seq) VALUES ('${seq}')`
   let conn = createConn()
   conn.query(sql, (err, results, fields) => {
     conn.end()
     if (!err) {
       res.send('ok')
     } else {
-      res.send('fail')
+      res.send('fali')
     }
   })
+})
+
+router.post('/add', function (req, res, next) {
+  let items = req.body.values
+  let state = req.body.stateval
+  let table = state.table 
+  let seq = state.seq 
+  let tmparr = []
+  for (let key in items) {
+    if (key !== 'process')
+    tmparr.push(`${key}='${items[key]}'`)
+  }
+  let sql = `UPDATE ${table} SET ${tmparr.join(',')} WHERE id>1 AND seq='${seq}'`
+  let conn = createConn()
+  new Promise((resolve, reject) => {
+    conn.query(sql, (err, results, fields) => {
+      if (!err) {
+        resolve('ok')
+      } else {
+        reject()
+      }
+    })
+  }).then(
+    () => {
+      sql = `INSERT INTO stateflash (seq,process,name,date,action,company) VALUES 
+            ('${state.seq}','${items.process.key}','${state.name}','${state.date}','${state.action}','${state.company}')`
+      conn.query(sql, (err, results, fields) => {
+        conn.end();
+        if (!err) {
+          res.send('ok')
+        } else {
+          res.send('fail')
+        }
+      })
+    }
+  ).catch(
+    () => {
+      conn.end()
+      res.send('fail')
+    }
+  )
 })
 
 module.exports = router
